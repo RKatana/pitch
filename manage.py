d56c12b82be7b826e32b2a6d7981e31b9b5a39ad
Raphael Katana <roduor41@gmail.com>
2018-11-20 10:24:33 +0300

    added app run functions

---
+++ manage.py
@@ -0,0 +1,19 @@
+import os
+from flask_script import Manager,Server
+from flask_migrate import Migrate,MigrateCommand
+from app import create_app,db
+from app.models import User,Coments,Pitches
+
+app = create_app('development')
+manager=Manager(app)
+manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def shell_context():
+    return dict(app=app,db=db,User=User)
+
+
+if __name__=='__main__':
+    manager.run()
